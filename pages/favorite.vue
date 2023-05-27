24f58d6c578f5c47f9c596f374931a66f3801411
rr <nikita.demchenko.rr@gmail.com>
2023-05-27 17:56:00 +0300

    favorite empty state

--- pages/favorite.vue
+++ pages/favorite.vue
@@ -2,16 +2,31 @@
 import { useFavoriteStore } from "~/store/favorite";
 
 const favoriteStore = useFavoriteStore();
+const favoriteList = favoriteStore.getFavoriteList();
 </script>
 
 <template>
   <div id="favorite" class="page-container">
     <div class="photos">
-      <Photo v-for="item in favoriteStore.getFavoriteList()" :photo="item">
-        <template #title>
-          <span class="favorite-photo-title">{{ item.title }}</span>
-        </template>
-      </Photo>
+      <template v-if="favoriteList && favoriteList.length">
+        <Photo v-for="item in favoriteList" :photo="item">
+          <template #title>
+            <span class="favorite-photo-title">{{ item.title }}</span>
+          </template>
+        </Photo>
+      </template>
+      <template v-else>
+        <div class="favorite-empty-state">
+          <fa
+            class="favorite-empty-state__icon"
+            :icon="['far', 'face-sad-cry']"
+          />
+          <h2 class="favorite-empty-state__title">Список избранного пуст</h2>
+          <div class="favorite-empty-state__additional">
+            Добавляйте изображения нажимая на звездочки
+          </div>
+        </div>
+      </template>
     </div>
   </div>
 </template>
--- plugins/fontawesome.ts
+++ plugins/fontawesome.ts
@@ -1,9 +1,9 @@
 import { library, config } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { faStar } from "@fortawesome/free-regular-svg-icons";
+import { faStar, faFaceSadCry } from "@fortawesome/free-regular-svg-icons";
 import { faPlus, faMinus, faStar as fasStart, faXmark } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faStar, faPlus, faMinus, fasStart, faXmark);
+library.add(faStar, faPlus, faMinus, fasStart, faXmark, faFaceSadCry);
 
 // This is important, we are going to let Nuxt worry about the CSS
 config.autoAddCss = false;
